83a6a603c905c5ad453583e2fe9c6349659f0c53
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-06-16 22:08:26 +0700

    More death messages

--- set.js
+++ set.js
@@ -1,8 +1,8 @@
 module.exports = {
     stats: {
-        deaths: /^([^ ]*) (?:was playing on a magma block too long|bắn bản thân|ran out of food, and died|dập mặt vào tường|fell from a high place|died in a unique way|blew up|thought they could swim forever|was burnt to a crisp|was squished to death|was slain by|thought standing in fire was a good idea)(.*)$/,
+        deaths: /^([^ ]*) (?:thougth lava was a hot tub|fell into the void|was playing on a magma block too long|bắn bản thân|ran out of food, and died|dập mặt vào tường|fell from a high place|died in a unique way|blew up|thought they could swim forever|was burnt to a crisp|was squished to death|was slain by|thought standing in fire was a good idea)(.*)$/,
         killBef: /^([^ ]*) (?:đã giết) ([^ ]*)(.*)$/,
         killAft: /^([^ ]*) (?:was slain by|was shot by a|was blown up by a) ([^ ]*)(.*)$/,
-        noStats: /^([^ ]*) (?:giết hại một con chó bằng) ([^ ]*)(.*)$/
+        noStats: /^([^ ]*) (?:Giết) ([^ ]*)(.*)$/
     }
 }
