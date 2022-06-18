723ef066eda877e79dad12125bc63bbac6b73c7f
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-06-18 21:39:33 +0700

    Fix not see uptime

--- igEvents/end.js
+++ igEvents/end.js
@@ -5,9 +5,6 @@ module.exports = {
     execute (bot) {
         console.log("Bot đã mất kết nối");
 
-        bot.exited = true;
-        bot.uptime = 0;
-
         setTimeout(() => {
             require('../bot.js').createBot();
         },  3 * 60 * 1000);
@@ -17,5 +14,7 @@ module.exports = {
         sendBotLog('disconnect', `Bot đã mất kết nối đến server. Kết nối lại sau 3 phút.\nThời gian trong hàng chờ là undefined. Thời gian trong server là ${getUptime(bot, 'vi')}`);
 
         sendGlobalChat('🏮 Bot đã mất kết nối đến server. 🏮');
+        bot.exited = true;
+        bot.uptime = 0;
     }
 }
