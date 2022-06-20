b3eded321b4adb0b1f0055032eaf2a9fdf0d4993
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-06-20 11:52:48 +0700

    death message

--- set.js
+++ set.js
@@ -1,8 +1,8 @@
 module.exports = {
     stats: {
-        deaths: /^([^ ]*) (?:died|drowned|was playing with poison|was pricked to death|flew into a wall|was playing with magic|was playing with tnt|thought lava was a hot tub|fell into the void|was playing on a magma block too long|bắn bản thân|ran out of food, and died|dập mặt vào tường|fell from a high place|died in a unique way|blew up|thought they could swim forever|was burnt to a crisp|was squished to death|was slain by|thought standing in fire was a good idea)(.*)$/,
+        deaths: /^([^ ]*) (?:discovered floor was lava|withered away|tried to swim in lava|hit the ground too hard|died|drowned|was playing with poison|was pricked to death|flew into a wall|was playing with magic|was playing with tnt|thought lava was a hot tub|fell into the void|was playing on a magma block too long|bắn bản thân|ran out of food, and died|fell from a high place|blew up|was burnt to a crisp|was squished to death)(.*)$/,
         killBef: /^([^ ]*) (?:killed) ([^ ]*)(.*)$/,
-        killAft: /^([^ ]*) (?:was burnt to a crisp whilst fighting|was slain by a|shot themselves with|was ganed up on by some|was shot by|was shot by a|was blown up by a) ([^ ]*)(.*)$/,
+        killAft: /^([^ ]*) (?:walked into danger zone due to|was pummeled by|was blown up by|was burnt to a crisp whilst fighting|was shot by) ([^ ]*)(.*)$/,
         noStats: /^([^ ]*) (?:Giết) ([^ ]*)(.*)$/
     }
 }
