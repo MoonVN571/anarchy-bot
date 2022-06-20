b1741000fd77af7b20cea888612ee32c8b7fad5d
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-06-20 12:24:29 +0700

    death msg

--- set.js
+++ set.js
@@ -2,7 +2,7 @@ module.exports = {
     stats: {
         deaths: /^([^ ]*) (?:discovered floor was lava|withered away|tried to swim in lava|hit the ground too hard|died|drowned|was playing with poison|was pricked to death|flew into a wall|was playing with magic|was playing with tnt|thought lava was a hot tub|fell into the void|was playing on a magma block too long|bắn bản thân|ran out of food, and died|fell from a high place|blew up|was burnt to a crisp|was squished to death)(.*)$/,
         killBef: /^([^ ]*) (?:killed) ([^ ]*)(.*)$/,
-        killAft: /^([^ ]*) (?:walked into danger zone due to|was pummeled by|was blown up by|was burnt to a crisp whilst fighting|was shot by) ([^ ]*)(.*)$/,
+        killAft: /^([^ ]*) (?:was slain by|walked into danger zone due to|was pummeled by|was blown up by|was burnt to a crisp whilst fighting|was shot by) ([^ ]*)(.*)$/,
         noStats: /^([^ ]*) (?:Giết) ([^ ]*)(.*)$/
     }
 }
