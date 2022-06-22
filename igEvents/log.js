826720544b085873b59426d75d68791d8a569996
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-06-23 00:28:14 +0700

    Update log.js

--- igEvents/log.js
+++ igEvents/log.js
@@ -2,7 +2,7 @@ const { sendBotLog, sendGlobalChat } = require('../functions/minecraft');
 
 module.exports = {
     name: 'spawn',
-    once: false,
+    once: true,
     execute (bot) {
         sendBotLog('join', `Bot đã kết nối đến server!`);
         sendGlobalChat(bot, '☘️ Bot đang vào server ☘️');
