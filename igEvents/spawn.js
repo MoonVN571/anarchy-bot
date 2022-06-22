c425e5e773ff11fe17bb9888d7515d08d6b784c1
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-06-22 10:07:11 +0700

    fix n

--- igEvents/spawn.js
+++ igEvents/spawn.js
@@ -2,7 +2,7 @@ const pt = require('../db/playtime');
 
 module.exports = {
     name: 'spawn',
-    once: true,
+    once: false,
     execute (bot) {
         bot.logged = true;
         if(bot.players.gamemode == 0) bot.mainServer = true;
