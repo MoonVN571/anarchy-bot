ba3bd4a79d8dac007bac13452a60d7e59b3c2df4
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-06-22 23:48:24 +0700

    Update spawn.js

--- igEvents/spawn.js
+++ igEvents/spawn.js
@@ -4,8 +4,6 @@ module.exports = {
     name: 'spawn',
     once: false,
     execute (bot) {
-        sendBotLog('join', `Bot đã kết nối đến server!`);
-        sendGlobalChat(bot, '☘️ Bot đang vào server ☘️');
         bot.logged = true;
         if(bot.player.gamemode == 0) bot.mainServer = true;
     }
