02d1d54585e03cacecc34463387a8636d66dae29
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-07-03 20:53:58 +0700

    Death messages

--- set.js
+++ set.js
@@ -1,6 +1,6 @@
 module.exports = {
     stats: {
-        deaths: /^([^ ]*) (?:thought they could swim forever|was squished to death|shot themselves|fell into the void|died in a unique way|đã cố gắng tập bơi trong lava|fell from a high place|was burnt to a crisp)(.*)$/,
+        deaths: /^([^ ]*) (?:was playing with magic|thought they could swim forever|was squished to death|shot themselves|fell into the void|died in a unique way|đã cố gắng tập bơi trong lava|fell from a high place|was burnt to a crisp)(.*)$/,
         killBef: /^([^ ]*) (?:killed) ([^ ]*)(.*)$/,
         killAft: /^([^ ]*) (?:was blown up by a|was slain by|đã bị|was shot by a|was slain by a) ([^ ]*)(.*)$/,
         noStats: /^([^ ]*) (?:murdered a dog using) ([^ ]*)(.*)$/
