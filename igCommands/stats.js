86bc4d399bfbde65827317bb2ced1c5fd467eda5
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-07 01:31:06 +0700

    Fix kdData return null

--- igCommands/stats.js
+++ igCommands/stats.js
@@ -22,8 +22,8 @@ module.exports = {
         if(!kdData) kd.create({username:name,kills:0,deaths:0});
 
 
-        let kills = kdData.kills || 0;
-        let deaths= kdData.deaths || 0;
+        let kills = kdData?.kills || 0;
+        let deaths= kdData?.deaths || 0;
         let kda = kills/deaths || 0.00;
 
         bot.sendMessage('whisper', name+' | Kills: ' + kills + " - Deaths: " + deaths + " K/D: " + kda.toFixed(2));
