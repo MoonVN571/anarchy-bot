f608ff5b2a9a711ce78fafa6374910e44c0e224d
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-08 21:58:04 +0700

    Mai fix

--- igEvent/check.js
+++ igEvent/check.js
@@ -4,11 +4,12 @@ const { getDorHMS } = require("../functions/utils");
 module.exports = {
     name: 'login',
     once: false,
-    execute (bot) {
+    execute (
         bot.logged = true;
         
         if(bot.player?.gamemode !== 0) return;
         bot.mainServer = true;
+        return;
         bot.uptime = Date.now();
 
         sendBotLog('queue', 'Đã đợi ' + getDorHMS((Date.now() - bot.queueStart)/1000, true, true) + " trước khi vào server.");
