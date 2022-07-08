ec58f8517a7c52d3d36506a7f3cc088f40fedafb
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-08 22:06:33 +0700

    Update and rename igEvent/check.js to igEvents/check.js

--- igEvents/check.js
+++ igEvents/check.js
@@ -2,15 +2,15 @@ const { sendBotLog } = require("../functions/minecraft");
 const { getDorHMS } = require("../functions/utils");
 
 module.exports = {
-    name: 'login',
+    name: 'spawn',
     once: false,
     execute (
         bot.logged = true;
         
         if(bot.player?.gamemode !== 0) return;
         if(!bot.mainServer) sendBotLog('queue', 'Đã đợi ' + getDorHMS((Date.now() - bot.queueStart)/1000, true, true) + " trước khi vào server");
+        
         bot.mainServer = true;
-
         bot.uptime = Date.now();
     }
 }
