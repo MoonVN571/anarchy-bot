adf7cb4f016efeb127fb30aec978e45eb650a56c
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-08 22:09:12 +0700

    Update check.js

--- igEvents/check.js
+++ igEvents/check.js
@@ -4,7 +4,7 @@ const { getDorHMS } = require("../functions/utils");
 module.exports = {
     name: 'spawn',
     once: false,
-    execute (
+    execute (bot) {
         bot.logged = true;
         
         if(bot.player?.gamemode !== 0) return;
