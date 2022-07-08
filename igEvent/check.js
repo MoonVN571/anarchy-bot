aa2e13082906801c752876804d16c0e50e5e8e34
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-08 22:00:36 +0700

    Update check.js

--- igEvent/check.js
+++ igEvent/check.js
@@ -8,10 +8,9 @@ module.exports = {
         bot.logged = true;
         
         if(bot.player?.gamemode !== 0) return;
+        if(!bot.mainServer) sendBotLog('queue', 'Đã đợi ' + getDorHMS((Date.now() - bot.queueStart)/1000, true, true) + " trước khi vào server");
         bot.mainServer = true;
-        return;
-        bot.uptime = Date.now();
 
-        sendBotLog('queue', 'Đã đợi ' + getDorHMS((Date.now() - bot.queueStart)/1000, true, true) + " trước khi vào server.");
+        bot.uptime = Date.now();
     }
 }
