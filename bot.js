0219a92ec6408bdefabf34aae27615b6e33972fd
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-07-14 11:42:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ let channel = {
     webhookLivechat: config.dev ? "995568425613152358" : "996999311135080466",
     webhookJoinMessage: config.dev ? "995585131123331112" : "995585131123331112",
     webhookJoin: config.dev ? "995585063582453862" : "996999642912931952",
-    webhookServer: config.dev ? "995585187889037312" : "996800472981712969",
+    webhookServer: config.dev ? "995585187889037312" : "996999899398799420",
     chat: config.dev ? "987204059838709780" : "986599157068361734",
     commands: config.dev ? "990104136018182154" : "987889094845689916"
 }
