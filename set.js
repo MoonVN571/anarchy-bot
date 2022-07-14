85ffeba534e5a2a6f273929a12dec0188f858c96
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-07-14 13:02:30 +0700

    Update set.js

--- set.js
+++ set.js
@@ -2,14 +2,14 @@ module.exports = {
     // regex để tính death message
     // credits to moobot
     stats: {
-        deaths: /^([^ ]*) (?:thought lava was a hot tub|withered away|killed themselves|was playing with poison|was playing with tnt|tried sleeping in the nether :kappa:|was playing on a magma block too long|was pricked to death|flew into a wall|blew up|ran out of food, and died|thought standing in fire was a good idea|died to a wither skull|was playing with magic|thought they could swim forever|was squished to death|shot themselves|fell into the void|died in a unique way|fell from a high place|was burnt to a crisp)(.*)$/,
+        deaths: /^([^ ]*) (?:blew up! They were playing around with an end-crystal!|thought lava was a hot tub|withered away|killed themselves|was playing with poison|was playing with tnt|tried sleeping in the nether :kappa:|was playing on a magma block too long|was pricked to death|flew into a wall|blew up|ran out of food, and died|thought standing in fire was a good idea|died to a wither skull|was playing with magic|thought they could swim forever|was squished to death|shot themselves|fell into the void|died in a unique way|fell from a high place|was burnt to a crisp)(.*)$/,
         killAft: /^([^ ]*) (?:tried playing with|stood too close to|was spat on by a|was ganed up on by some|tried climbing to greater heights and fell off|was ganged up on by some|was pushed off a high place by|was shot by|was blown up by a|was slain by) ([^ ]*)(.*)$/,
         noStats: /^([^ ]*) (?:murdered a dog using|killed) ([^ ]*)(.*)$/
     },
     // Random send message
     messageArray: [
-        'Xem tất cả lệnh của bot tại https://mo0nbot.tk/',
-        'Chat trong server ở discord tại server: https://discord.mo0nbot.tk/',
+        ' Xem tất cả lệnh của bot tại https://mo0nbot.tk/',
+        ' Chat trong server ở discord tại server: https://discord.mo0nbot.tk/',
     ],
     // Text khi k có player data
     notFoundPlayers: 'Không tìm thấy người chơi này.',    
