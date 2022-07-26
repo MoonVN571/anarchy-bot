6fdc13372720c83c7905af551bf3674e3820eb9c
MoonVN571 <hackerdmpicer3213212@gmail.com>
2022-07-26 11:18:00 +0700

    Connect message

--- igEvents/playerJoin.js
+++ igEvents/playerJoin.js
@@ -21,7 +21,7 @@ module.exports = {
             seenData.save();
         }
 
-        if (getPlayersList(bot).indexOf(player.username) > -1) return;
+        if (bot.data.countPlayers <= getPlayersList(bot).length) return;
 
         sendCustomMessage('connect', player.username + ' đã tham gia vào server.');
     }
