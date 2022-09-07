12fb8064feecfcdfb36f4120038b186af15f3fd5
MoonVNN <60611504+MoonVN571@users.noreply.github.com>
2022-09-07 17:58:51 +0700

    Update kicked.js

--- igEvents/kicked.js
+++ igEvents/kicked.js
@@ -4,8 +4,8 @@ module.exports = {
     name: 'kicked',
     
     execute (bot, reason, loggedIn) {
-        if(reason.toString().includes("Reconnect 2 more times to enter!")) bot.data.fastReconnect = true;
-        if(reason.toString().includes("Reconnect 1 more times to enter!")) bot.data.fastReconnect = true;
+        if(reason.toString().includes("Kết nối 2")) bot.data.fastReconnect = true;
+        if(reason.toString().includes("Kết nối 1")) bot.data.fastReconnect = true;
         if(reason.toString().includes("As this is your first time playing")) bot.data.fastReconnect = true;
         
         console.log(reason, loggedIn);
