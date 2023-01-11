bd733ff2176be96cc2738904159b01b139f002c8
MoonVN571 <hackerdmpicer3213212@gmail.com>
2023-01-11 19:36:24 +0700

    features

--- controllers/data.js
+++ controllers/data.js
@@ -1,25 +1,24 @@
-const { servers, stats } = require('../api.json');
+const { servers, type } = require('../api.json');
 const dbList = require('../index');
 module.exports = async (req, res) => {
     const username = req.params?.username;
-    const stat = req.params?.stats;
+    const stats = req.params?.stats;
     const server = req.params?.server;
     const key = req.query?.apikey || req.query?.key;
     const api = await dbList.find(db => db.name == 'anarchyvn').collection('apikeys').findOne({ key: key });
     if (!api) return res.send({
         statusCode: 404, msg: 'Invalid api key!'
     });
-    console.log('[', new Date().toLocaleString(), `] ${key} | ${server} - ${stat} - ${username}`);
+    console.log(`[${new Date().toLocaleString()}] ${key} | ${server} - ${stats} - ${username}`);
     if (servers.indexOf(server) == -1)
         return res.send({ statusCode: 404, msg: `Server '${server}' not found on server!` });
-    if (stats.indexOf(stat) == -1)
-        return res.send({ statusCode: 404, msg: `Data '${stat}' not found on server!` });
-    const db = await dbList.find(db => db.name == server).collection(stat == 'stats' ? 'stats2' : stat + 's')
-        .findOne({
-            username: {
-                $regex: new RegExp(`^${username}$`), $options: 'i'
-            },
-        }, { _id: 0, __v: 0 });
+    if (type.indexOf(stats) == -1)
+        return res.send({ statusCode: 404, msg: `Data '${stats}' not found on server!` });
+    const db = await dbList.find(db => db.name == server).collection(stats == 'stats' ? 'stats2' : stats + 's').findOne({
+        username: {
+            $regex: new RegExp(`^${username}$`), $options: 'i'
+        },
+    });
     if (!username || !db)
         return res.send({ statusCode: 404, msg: `Username '${username}' not found on server!` });
     let data = {};
