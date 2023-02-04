b2b372a7b0af3a906c5c1dfabccc49385b98c202
MoonVN571 <hackerdmpicer3213212@gmail.com>
2023-02-04 12:16:19 +0700

    ratelimit

--- express.js
+++ express.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
 const data_route = require('./routes/data.route');
+const { rateLimit } = require('express-rate-limit');
 const port = process.env.PORT || 80;
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 50,
+    standardHeaders: true,
+    legacyHeaders: false,
+    message: (req, res) => {
+        res.send({ status: false, msg: 'Too many requests try again later!' });
+    },
+})
+app.use(limiter);
 app.all('*', (req, res, next) => {
     if (req.hostname.startsWith('api.')) next();
 });
