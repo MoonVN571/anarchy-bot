ba8748552ac2cc3b4eafb9bec663a87588e60705
MoonVN571 <hackerdmpicer3213212@gmail.com>
2023-02-13 20:06:53 +0700

    typo lmao

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const connectList = ['anarchyvn', '2b2c'];
+const connectList = require('./api.json').servers;
 const dbList = [];
 connectList.forEach(async data => {
+    require('dotenv').config();
     const db = mongoose.createConnection(process.env.MONGO_STRING + '/' + data);
     dbList.push(db);
 });
